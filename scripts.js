cabac61e12256ae153a53abfe9c40fb3dca1ae60
gmonteiro13 <gscmonteiro@gmail.com>
2021-06-06 17:42:03 -0300

    Adds Utils object and formatCurrency method

--- scripts.js
+++ scripts.js
@@ -59,12 +59,33 @@ const DOM = {
   },
   /* Fill the table row created above with the transaction data */
   innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+    const amount = Utils.formatCurrency(transaction.amount)
+
     const html = `
             <td class="description">${transaction.description}</td>
-            <td class="expense">${transaction.amount}</td>
+            <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td><img src="./assets/minus.svg" alt="Remover transação"></td>
     `
     return html
   }
 }
+
+const Utils = {
+  formatCurrency(value){
+    const signal = Number(value) < 0 ? "-" : ""
+    value = String(value).replace(/\D/g, "")
+    value = Number(value) / 100
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+    return signal + value
+  }
+}
+
+transactions.forEach(function(transaction){
+  DOM.addTransaction(transaction)
+})
