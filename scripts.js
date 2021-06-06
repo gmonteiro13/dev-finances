d52a64c8c64c844d260c4eeb740095c847828163
gmonteiro13 <gscmonteiro@gmail.com>
2021-06-06 17:01:08 -0300

    Adds method to make new transactions and put it into the html table. Deletes the transactions examples of index.html

--- scripts.js
+++ scripts.js
@@ -42,3 +42,29 @@ const Transaction = {
 
   }
 }
+
+/* Object DOM created to manage the actions (transactions, etc) */
+const DOM = {
+  /* This will be the transactions container (will be added to the body of the table) */
+  transactionsContainer: document.querySelector('#data-table tbody'),
+
+  /* Adds a new transaction */
+  addTransaction(transaction, index) {
+    /* Creates a new transaction table row (tr) */
+    const tr = document.createElement('tr')
+    /* Fill the transaction table row (tr) using the innerHTMLTransaction method (line 60) */
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+    /* Appends the new transaction table row to the table body */
+    DOM.transactionsContainer.appendChild(tr)
+  },
+  /* Fill the table row created above with the transaction data */
+  innerHTMLTransaction(transaction) {
+    const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="expense">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td><img src="./assets/minus.svg" alt="Remover transação"></td>
+    `
+    return html
+  }
+}
